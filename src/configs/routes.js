a396fd9db30c5543fc19ace28b18105e5f0ed4d3
quanscheng <quanscheng@163.com>
2022-03-23 15:28:42 +0800

    更改了组件名字

--- src/configs/routes.js
+++ src/configs/routes.js
@@ -1,10 +1,10 @@
 import { Suspense, lazy } from "react";
 
-import { Home } from "@/pages/Home";
+import { Home } from "@/pages/Home/Home";
 import { Login } from "@/pages/Login/Login";
 import { _Url } from "./routes.url";
 
-const Child = lazy(() => import("@/pages/Child")); // 懒加载必须默认导出
+const Child = lazy(() => import("@/pages/Child/Child")); // 懒加载必须默认导出
 
 // react-router v6 迁移: 之前写法是 component 现在的写法是 element
 export const routes = [
