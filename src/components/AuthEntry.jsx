19e92c115c19f2c4923ed07c86363b47d24314a1
quanscheng <quanscheng@163.com>
2022-04-13 23:43:34 +0800

    判断token

---
+++ src/components/AuthEntry.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from "react";
+
+const AuthEntry = ({ children }) => {
+  useEffect(() => {
+    // 判断token
+  }, []);
+  return children;
+};
+
+export default AuthEntry;
